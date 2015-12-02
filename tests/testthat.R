67030b33f973bb74e4f5ec2c27975f444dbd33b0
Elizabeth Purdom <epurdom@stat.berkeley.edu>
2015-12-01 22:16:25 -0800

    move start of test stuff to feature branch.

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,10 +2,3 @@ library(testthat)
 library(clusterCells)
 
 test_check("clusterCells")
-expect_error(compareChoices(simData, ks=5,
-                             alphas=c(0.1), findBestK=c(TRUE,FALSE),
-                             sequential=c(FALSE),
-                             subsample=c(TRUE),
-                             removeSil=c(TRUE,FALSE), clusterMethod=c("pam","tight","hierarchical"),
-                             clusterDArgs = list(minSize = 5,kRange=2:15),ncores=1,random.seed=48120)
-             ,"must provide k in subsampleArgs for those with findBestK=TRUE and sequential=FALSE")
