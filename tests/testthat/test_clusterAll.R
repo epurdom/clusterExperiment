406ff2e465a2da57ed2001da2242a674eda3e52c
Davide Risso <risso.davide@gmail.com>
2016-04-01 11:23:16 -0700

    Tests for clusterAll

---
+++ tests/testthat/test_clusterAll.R
@@ -0,0 +1,22 @@
+library(clusterCells)
+data(simData)
+
+test_that("`clusterAll` works with matrix, ClusterCells objects, and
+          SummarizedExperiments", {
+            clustNothing <- clusterAll(t(simData), clusterFunction="pam",
+                                       subsample=FALSE, sequential=FALSE,
+                                       clusterDArgs=list(k=3))
+            se <- SummarizedExperiment(t(simData))
+            clustNothing2 <- clusterAll(se, clusterFunction="pam",
+                                       subsample=FALSE, sequential=FALSE,
+                                       clusterDArgs=list(k=3))
+            clustNothing3 <- clusterAll(clustNothing2, clusterFunction="pam",
+                                       subsample=FALSE, sequential=FALSE,
+                                       clusterDArgs=list(k=3))
+
+            expect_equal(clustNothing, clustNothing2)
+            expect_equal(clustNothing, clustNothing3)
+
+            expect_is(clustNothing, "ClusterCells")
+            expect_is(clustNothing, "SummarizedExperiment")
+          })
