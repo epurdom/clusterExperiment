daee4240b231b423657c6dd28fdbf4f9f7bc8b55
epurdom <epurdom@stat.berkeley.edu>
2016-04-14 13:25:21 -0700

    move function into internal functions and deleted file

--- R/internalFunctions.R
+++ R/internalFunctions.R
@@ -197,6 +197,23 @@
 	return(clust.id)
 }
 
-
+####
+#Convert to object used by phylobase so can navigate easily 
+.makePhylobaseTree<-function(x,type){
+    type<-match.arg(type,c("hclust","dendro"))
+    if(type=="hclust"){
+        #first into phylo from ape package
+        tempPhylo<-try(ape::as.phylo(x),FALSE)
+        if(inherits(tempPhylo, "try-error")) stop("the hclust object cannot be converted to a phylo class with the methods of the 'ape' package.")
+    }
+    if(type=="dendro"){
+        tempPhylo<-try(dendextend::as.phylo.dendrogram(x),FALSE)
+        if(inherits(tempPhylo, "try-error")) stop("the dendrogram object cannot be converted to a phylo class with the methods of 'dendextend' package. Check that you gave simple hierarchy of clusters, and not one with fake data per sample")
+    }
+    phylo4Obj<-try(as(tempPhylo,"phylo4"),FALSE) 
+    if(inherits(phylo4Obj, "try-error")) stop("the internally created phylo object cannot be converted to a phylo4 class. Check that you gave simple hierarchy of clusters, and not one with fake data per sample")
+    phylobase::nodeLabels(phylo4Obj)<-paste("Node",1:phylobase::nNodes(phylo4Obj),sep="")
+    return(phylo4Obj)
+}
 
 
