4845829bec46f177a03806f7d6038a640b800986
Davide Risso <risso.davide@gmail.com>
2016-04-20 17:38:49 -0700

    Moved plotCoClustering in R/plotHeatmap.R to affect order of man page

--- R/plotHeatmap.R
+++ R/plotHeatmap.R
@@ -225,9 +225,9 @@ setMethod(
 setMethod(
   f = "plotHeatmap",
   signature = signature(data = "ClusterExperiment"),
-  definition = function(data, 
-                        clusterSamplesData=c("hclust","dendrogramValue","orderSamplesValue","primaryCluster"), 
-                        clusterFeaturesData=c("mostVar","all","PCA"), nFeatures=NULL, 
+  definition = function(data,
+                        clusterSamplesData=c("hclust","dendrogramValue","orderSamplesValue","primaryCluster"),
+                        clusterFeaturesData=c("mostVar","all","PCA"), nFeatures=NULL,
                         visualizeData=c("transformed","centeredAndScaled","original"),
                         whichClusters= c("primary","pipeline","all","none"),
                         sampleData=NULL,clusterFeatures=TRUE,
@@ -574,3 +574,31 @@ setMethod(
       invisible(list(aheatmapOut=out,sampleData=annCol,clusterLegend=clusterLegend,breaks=breaks))
     }
 )
+
+#' @rdname plotHeatmap
+#' @name plotHeatmap
+#' @aliases plotHeatmap plotCoClustering
+#'
+#' @details \code{plotCoClustering} is a convenience function to plot the heatmap
+#' of the co-clustering matrix stored in the \code{coClustering} slot of a
+#' \code{ClusterExperiment} object.
+#'
+setMethod(
+  f = "plotCoClustering",
+  signature = "ClusterExperiment",
+  definition = function(data, ...){
+    if(nrow(data@coClustering)==0) stop("coClustering slot is empty")
+    fakeCE<-clusterExperiment(data@coClustering,
+                              clusterMatrix(data),
+                              transformation=function(x){x},
+                              clusterInfo=clusterInfo(data),
+                              clusterType=clusterType(data)
+    )
+    clusterLegend(fakeCE)<-clusterLegend(data)
+    orderSamples(fakeCE)<-orderSamples(data)
+    colData(fakeCE)<-colData(data)
+    fakeCE@dendro_samples<-data@dendro_samples
+    fakeCE@primaryIndex<-data@primaryIndex
+    validObject(fakeCE) #just in case screwed something up
+    plotHeatmap(fakeCE,isSymmetric=TRUE,whichFeatures="all",...)
+  })
