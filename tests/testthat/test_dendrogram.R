1e913386b1b5416dcf18c8b6498b89920a31c875
Elizabeth Purdom <epurdom@stat.berkeley.edu>
2016-04-29 11:30:40 -0700

    fixed error handling in makeDendrogram

--- tests/testthat/test_dendrogram.R
+++ tests/testthat/test_dendrogram.R
@@ -16,6 +16,15 @@ test_that("`makeDendrogram` works with matrix, ClusterExperiment objects", {
     expect_equal(nobs(makeDendrogram(mat, primaryCluster(cc), unassigned="remove")$samples),
                  length(primaryCluster(cc))-2)
 
+    #test proper error if only single cluster:
+    fakeCluster<-rep(1,nSamples(cc))
+    cc1<-addClusters(cc,fakeCluster)
+    primaryClusterIndex(cc1)<-3
+    expect_error(makeDendrogram(cc1),"Only 1 cluster given")
+    fakeCluster[c(1:2)]<- -1
+    cc1<-addClusters(cc,fakeCluster)
+    primaryClusterIndex(cc1)<-3
+    expect_error(makeDendrogram(cc1),"Only 1 cluster given")
 })
 
 test_that("`makeDendrogram` preserves the colData and rowData of SE", {
