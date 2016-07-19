bb7e21aaa89c814670d548731c6c13239ccb2e9f
Davide Risso <risso.davide@gmail.com>
2016-07-19 11:49:41 -0700

    Export nFeatures and nSamples generics

--- R/AllGenerics.R
+++ R/AllGenerics.R
@@ -104,6 +104,7 @@ setGeneric(
 #' features.
 #' @param x an object that describes a dataset or a model.
 #' @return the number of features.
+#' @export
 setGeneric(
   name = "nFeatures",
   def = function(x) {
@@ -117,6 +118,7 @@ setGeneric(
 #' samples.
 #' @param x an object that describes a dataset or a model.
 #' @return the number of samples.
+#' @export
 setGeneric(
   name = "nSamples",
   def = function(x) {
