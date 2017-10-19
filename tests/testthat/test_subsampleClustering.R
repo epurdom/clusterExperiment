53a96b6a748d189fea949e575061a493364f9654
Elizabeth Purdom <epurdom@stat.berkeley.edu>
2017-10-19 15:00:16 +0200

    add another test about large dataset option with RSEC

--- tests/testthat/test_subsampleClustering.R
+++ tests/testthat/test_subsampleClustering.R
@@ -21,4 +21,12 @@ test_that("`subsampleClustering` works", {
     set.seed(4897)
     subInSample <- subsampleClustering(x=mat,clusterArgs=list(k=3), clusterFunction="pam",  classifyMethod=c("InSample"),largeDataset=FALSE, resamp.num = 100, samp.p = 0.7,ncores=1)
 	expect_identical(subInSampleLarge,subInSample)
+	
+	#test in passing to subsampleArgs
+	rsecOut1<-RSEC(x=mat, isCount=FALSE,dimReduce="none",k0s=4:5,clusterFunction="tight", alphas=0.1,dendroReduce="none",
+       subsampleArgs=list(resamp.num=5, largeDataset=TRUE),random.seed=495)
+   	rsecOut2<-RSEC(x=mat, isCount=FALSE,dimReduce="none",k0s=4:5,clusterFunction="tight", alphas=0.1,dendroReduce="none",
+          subsampleArgs=list(resamp.num=5, largeDataset=FALSE),random.seed=495)
+	expect_identical(clusterMatrix(rsecOut1),clusterMatrix(rsecOut2))
+  
  })
