7153ee35f83285994f301331422ccf369a2cd732
Elizabeth Purdom <epurdom@stat.berkeley.edu>
2017-11-02 13:50:18 +0100

    file for reading max of memory

---
+++ tests/checkClusterMany/readMemoryLog.R
@@ -0,0 +1,22 @@
+args<-commandArgs(TRUE)
+if(length(args)==0) stop("Usage should be 'RScript readMemoryLog.R <fileName>' where <fileName> is the name of the file with the memory log.")
+file<-args[1]
+file<-"memoryLogger_StandardSmall.txt"
+log<-read.table(file,header=FALSE,fill=TRUE,stringsAsFactors=FALSE)
+nms<-head(unlist(log[1,]),-1)
+log<-log[log[,1]=="Mem:",-1]
+names(log)<-nms
+
+#from:
+#https://stackoverflow.com/questions/10910688/converting-kilobytes-megabytes-etc-to-bytes-in-r
+convb <- function(x){
+  ptn <- "(\\d*(.\\d+)*)(.*)"
+  num  <- as.numeric(sub(ptn, "\\1", x))
+  unit <- sub(ptn, "\\3", x)             
+  unit[unit==""] <- "1" 
+
+  mult <- c("1"=1, "K"=1024, "M"=1024^2, "G"=1024^3)
+  num * unname(mult[unit])
+}
+maxMemUsed<-log$used[which.max(convb(log$used))]
+print(maxMemUsed,stdout())
--- tests/checkClusterMany/recordMemory.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-
-echo "      date     time $(free -m | grep total | sed -E 's/^    (.*)/\1/g')"
-while true; do
-    echo "$(date '+%Y-%m-%d %H:%M:%S') $(free -m | grep Mem: | sed 's/Mem://g')"
-    sleep 1
-done
