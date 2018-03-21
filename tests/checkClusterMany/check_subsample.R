e39120d20d02e50c153d2f5b87fc31309d418452
Davide Risso <dar2062@med.cornell.edu>
2018-03-21 10:07:57 -0400

    check subsampling

---
+++ tests/checkClusterMany/check_subsample.R
@@ -0,0 +1,283 @@
+library(scran)
+library(zinbwave)
+library(Rtsne)
+library(igraph)
+library(profvis)
+library(devtools)
+library(testthat)
+library(pryr)
+
+load("~/git/10x_utils/processed_171109/combined_filtered_all_batches.rda")
+load("~/git/10x_utils/processed_171109/combined_zinbwave_all_batches.rda")
+
+W <- getW(zinb)
+rownames(W) <- colnames(filtered)
+colnames(W) <- paste0("W", 1:10)
+
+# trap_wrap <- function(x, k, steps = 4, ...) {
+#   snn <- buildSNNGraph(x, k = k, d = NA, transposed = FALSE)
+#   res <- cluster_walktrap(snn, steps = steps)
+#   return(res$membership)
+# }
+#
+# internalFunctionCheck(trap_wrap, inputType = "X", algorithmType = "K",
+#                       outputType="vector")
+#
+# SNN <- ClusterFunction(trap_wrap, inputType = "X", algorithmType = "K",
+#                        outputType="vector")
+
+
+## first let's make sure that we get the same results, then we can memory proof them
+
+library(clusterExperiment)
+set.seed(123)
+system.time(master <- clusterSingle(t(W), subsample = TRUE, sequential = FALSE,
+                        mainClusterArgs = list(clusterFunction = "hierarchical01",
+                                               clusterArgs = list(alpha = 0.9)),
+                        subsampleArgs = list(clusterFunction = "kmeans",
+                                             clusterArgs = list(k = 5),
+                                             samp.p = 0.1,
+                                             resamp.num = 10)))
+
+set.seed(123)
+profvis(master <- clusterSingle(t(W), subsample = TRUE, sequential = FALSE,
+                                    mainClusterArgs = list(clusterFunction = "hierarchical01",
+                                                           clusterArgs = list(alpha = 0.9)),
+                                    subsampleArgs = list(clusterFunction = "kmeans",
+                                                         clusterArgs = list(k = 5),
+                                                         samp.p = 0.1,
+                                                         resamp.num = 10)))
+
+# set.seed(123)
+# profvis(lds <- clusterSingle(t(W), subsample = TRUE, sequential = FALSE,
+#                                     mainClusterArgs = list(clusterFunction = "hierarchical01",
+#                                                            clusterArgs = list(alpha = 0.9)),
+#                                     subsampleArgs = list(clusterFunction = "kmeans",
+#                                                          clusterArgs = list(k = 5),
+#                                                          samp.p = 0.1,
+#                                                          resamp.num = 10,
+#                                                          largeDataset = TRUE)))
+#
+# expect_equivalent(master, lds)
+
+detach("package:clusterExperiment")
+load_all()
+set.seed(123)
+system.time(test <- clusterSingle(t(W), subsample = TRUE, sequential = FALSE,
+                        mainClusterArgs = list(clusterFunction = "hierarchical01",
+                                               clusterArgs = list(alpha = 0.9)),
+                        subsampleArgs = list(clusterFunction = "kmeans",
+                                             clusterArgs = list(k = 5),
+                                             samp.p = 0.1,
+                                             resamp.num = 10)))
+
+expect_equal(master, test)
+
+set.seed(123)
+profvis(test <- clusterSingle(t(W), subsample = TRUE, sequential = FALSE,
+                              mainClusterArgs = list(clusterFunction = "hierarchical01",
+                                                     clusterArgs = list(alpha = 0.9)),
+                              subsampleArgs = list(clusterFunction = "kmeans",
+                                                   clusterArgs = list(k = 5),
+                                                   samp.p = 0.1,
+                                                   resamp.num = 10)))
+
+set.seed(123)
+profvis(test3 <- clusterSingle(t(W), subsample = TRUE, sequential = FALSE,
+                               mainClusterArgs = list(clusterFunction = "hierarchical01",
+                                                      clusterArgs = list(alpha = 0.9)),
+                               subsampleArgs = list(clusterFunction = "kmeans",
+                                                    clusterArgs = list(k = 5),
+                                                    samp.p = 0.1,
+                                                    resamp.num = 50)))
+
+
+classY <- sample(1:5, 20000, replace=TRUE)
+profvis({
+  D2 <- lapply(seq_len(length(classY)), function(i) classY == classY[i])
+  D1 <- outer(classY, classY, function(a, b) a==b)
+  D3 <- unlist(D2)
+  D4 <- as.vector(D1)
+})
+
+
+master <- clusterSingle(t(W), subsample = TRUE, sequential = FALSE,
+                        mainClusterArgs = list(clusterFunction = "hierarchical01",
+                                               clusterArgs = list(alpha = 0.9)),
+                        subsampleArgs = list(clusterFunction = "kmeans",
+                                             clusterArgs = list(k = 5),
+                                             samp.p = 0.1,
+                                             resamp.num = 10, largeDataset=TRUE))
+
+
+#############################
+
+otherIds<-function(idx,clustVec,clustLeng){
+  m<-which(clustVec==idx)
+  if(length(m)>1) stop("ids clustered in more than one cluster")
+  if(length(m)==0) return(NA) #sample not ever clustered
+  if(length(m)==1){
+    ends<-cumsum(clustLeng)
+    begins<-cumsum(c(1,head(clustLeng,-1)))
+    whCluster<-which(m<=ends & m>=begins)
+    if(length(whCluster)>1 | length(whCluster)==0) stop("error in coding: finding range of clusterids")
+    return(clustVec[seq(begins[whCluster],ends[whCluster],by=1)])
+  }
+}
+
+otherIds2 <- function(idx, classX) {
+  return(which(classX == classX[idx]))
+}
+
+otherIds3 <- Vectorize(otherIds2, vectorize.args = "idx", SIMPLIFY = FALSE)
+
+searchForPairs<-function(ii,clusterList){
+
+  #get list of those indices sample ii was sampled
+  whHave<-which(sapply(clusterList,function(ll){ii%in%ll$clusterIds}))
+
+  #calculate number of times sampled with (denominator)
+  sampledWithTab<-table(unlist(lapply(clusterList[whHave],.subset2,"clusterIds")))
+
+  #get those indices clustered with and tabulate
+  clusterWith<-lapply(clusterList[whHave],function(ll){
+    otherIds(idx=ii,clustVec=ll$clusterIds,clustLeng=ll$clusterLengths)
+  })
+
+  clusterWithTab<-table(unlist(clusterWith))
+  jointNames<-as.character(1:N)
+  whLower<-which(as.integer(as.numeric(jointNames))<ii)
+
+  return(as.integer(clusterWithTab[jointNames][whLower])/as.integer(sampledWithTab[jointNames][whLower]))
+  #old code that made Nx3 matrix and subset it:
+  #             #make a N x 3 matrix summarizing the results for all indices
+  # out<-cbind(idx=as.integer(as.numeric(jointNames)), together=as.integer(clusterWithTab[jointNames]), total=as.integer(sampledWithTab[jointNames]))
+  # #keep only those in the lower triangle
+  #             out<-out[out[,"idx"]<ii,,drop=FALSE]
+  #             return(out[,"together"]/out[,"total"])
+
+  #thoughts about alternative code if not need save NxN matrix...
+  #jointNames<-names(sampledWithTab) #if manage to not save NxN matrix, could use this to return only those that actually present
+  #out<-out[!is.na(out[,"together"]),,drop=FALSE] #if manage to not save NxN matrix, could use this to return only those that actually present; but then need to not return proportions, but something else.
+}
+
+searchForPairs2<-function(ii, clusterList, ncores = 1){
+
+  #get list of those indices sample ii was sampled
+  whHave<-which(sapply(clusterList,function(ll){ii%in%ll$clusterIds}))
+
+  #calculate number of times sampled with (denominator)
+  sampledWithTab<-table(unlist(lapply(clusterList[whHave],.subset2,"clusterIds")))
+
+  #get those indices clustered with and tabulate
+  if(ncores == 1) {
+    clusterWith<-lapply(clusterList[whHave],function(ll){
+      otherIds2(idx=ii, classX = ll$classX)
+    })
+  } else {
+    clusterWith<-parallel::mclapply(clusterList[whHave],function(ll){
+      otherIds2(idx=ii, classX = ll$classX, mc.cores = ncores)
+    })
+  }
+
+  clusterWithTab<-table(unlist(clusterWith))
+  jointNames<-as.character(1:N)
+  whLower<-which(as.integer(as.numeric(jointNames))<ii)
+
+  return(as.integer(clusterWithTab[jointNames][whLower])/as.integer(sampledWithTab[jointNames][whLower]))
+  #old code that made Nx3 matrix and subset it:
+  #             #make a N x 3 matrix summarizing the results for all indices
+  # out<-cbind(idx=as.integer(as.numeric(jointNames)), together=as.integer(clusterWithTab[jointNames]), total=as.integer(sampledWithTab[jointNames]))
+  # #keep only those in the lower triangle
+  #             out<-out[out[,"idx"]<ii,,drop=FALSE]
+  #             return(out[,"together"]/out[,"total"])
+
+  #thoughts about alternative code if not need save NxN matrix...
+  #jointNames<-names(sampledWithTab) #if manage to not save NxN matrix, could use this to return only those that actually present
+  #out<-out[!is.na(out[,"together"]),,drop=FALSE] #if manage to not save NxN matrix, could use this to return only those that actually present; but then need to not return proportions, but something else.
+}
+
+searchForPairs3 <- Vectorize(searchForPairs2, vectorize.args = "ii", SIMPLIFY = FALSE)
+
+# ## For each subsample (list) find the pairs
+# searchForPairs4 <- function(N, classX, clusterIds) {
+#
+#   clusterWith <- otherIds3(seq_len(N), classX)
+#
+# }
+
+set.seed(112)
+N <- 100
+classX <- sample(1:10, N, replace = TRUE)
+classY <- sample(1:10, N, replace = TRUE)
+clusterIdList1 <- tapply(1:N, classX, identity, simplify=FALSE)
+clusterIds1 <- unname(unlist(clusterIdList1))
+clusterLengths1 <- as.integer(tapply(1:N, classX, length))
+clusterIdList2 <- tapply(1:N, classY, identity, simplify=FALSE)
+clusterIds2 <- unname(unlist(clusterIdList2))
+clusterLengths2 <- as.integer(tapply(1:N, classY, length))
+
+clusterList <- list(list(clusterIds=clusterIds1, clusterLengths=clusterLengths1, classX = classX),
+              list(clusterIds=clusterIds2, clusterLengths=clusterLengths2, classX = classY))
+
+expect_equal(lapply(1:N, otherIds2, classX), lapply(1:N, otherIds, clusterIds1, clusterLengths1))
+expect_equal(lapply(1:N, otherIds, clusterIds1, clusterLengths1), otherIds3(1:N, classX))
+
+expect_equal(lapply(2:N,function(jj){searchForPairs(jj,clusterList=clusterList)}),
+             lapply(2:N,function(jj){searchForPairs2(jj,clusterList=clusterList)}))
+
+expect_equal(lapply(2:N,function(jj){searchForPairs(jj,clusterList=clusterList)}), searchForPairs3(2:N, clusterList))
+
+pairList <- searchForPairs3(2:N, clusterList)
+
+#Create NxN matrix
+Dbar<-matrix(0,N,N)
+Dbar[upper.tri(Dbar, diag = FALSE)]<-unlist(pairList)
+Dbar<-Dbar+t(Dbar)
+Dbar[is.na(Dbar)]<-0
+diag(Dbar)<-1
+Dbar_lower <- Dbar[lower.tri(Dbar, diag=FALSE)]
+
+clusterMat <- sapply(clusterList, function(x) x$classX)
+Dbar2 <- search_pairs(t(clusterMat))
+Dbar2_lower <- Dbar2[lower.tri(Dbar2, diag=FALSE)]
+
+expect_equal(Dbar_lower, Dbar2_lower)
+
+library(microbenchmark)
+
+microbenchmark(
+  fun1={lapply(2:N,function(jj){searchForPairs(jj,clusterList=clusterList)})},
+  fun2={searchForPairs3(2:N, clusterList)},
+  fun3={search_pairs(t(clusterMat))}
+)
+
+# tapply(1:N, mat[,1], identity, simplify = FALSE)
+library(cluster)
+
+set.seed(1222)
+N <- 25
+x <- rbind(cbind(rnorm(10,0,0.5), rnorm(10,0,0.5)),
+           cbind(rnorm(15,5,0.5), rnorm(15,5,0.5)))
+mat <- t(sapply(2:4, function(k) pam(x, k=k)$clustering))
+mat
+mat2 <- mat[,1:3]
+mat2[2,2] <- NA
+mat2
+mat3 <- mat2
+mat3[2,3] <- NA
+mat3
+mat4 <- mat2
+mat4[1,3] <- NA
+mat4
+
+sourceCpp("src/search_pairs.cpp")
+search_pairs(mat2)
+search_pairs(mat3)
+search_pairs(mat4)
+
+
+
+
+
+
