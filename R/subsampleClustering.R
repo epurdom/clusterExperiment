01498db0199fb7d16df7a6bb7db9eb6562a65533
Elizabeth Purdom <epurdom@stat.berkeley.edu>
2018-03-21 18:00:31 +0100

    quick update

--- R/subsampleClustering.R
+++ R/subsampleClustering.R
@@ -241,7 +241,7 @@ definition=function(clusterFunction, x=NULL,diss=NULL,distFunction=NA,clusterArg
         # 1) one vector of length na.omit(classX) of the original indices, where ids in clusters are adjacent in the vector and
         # 2) another vector of length K indicating length of each cluster (allows to decode where the cluster stopes in the above vector),
         # What does this do with NAs? Removes them -- not included.
-        clusterIds<-unlist(tapply(1:N,classX,function(x){x},simplify=FALSE))
+        clusterIds<-unlist(tapply(1:N,classX,function(x){x},simplify=FALSE),use.names=FALSE)
         clusterLengths<-tapply(1:N,classX,length)
         return(list(clusterIds=clusterIds,clusterLengths=clusterLengths))
       }
