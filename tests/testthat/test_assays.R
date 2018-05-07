4fa593a9dc75fadaae042a0bafa44e7d0cc39d22
Davide Risso <dar2062@med.cornell.edu>
2018-05-07 17:17:16 -0400

    Smaller tests for heatmap

--- tests/testthat/test_assays.R
+++ tests/testthat/test_assays.R
@@ -115,7 +115,9 @@ test_that("plotting works wih non default assays", {
   expect_silent(plotFeatureBoxplot(object=multi_cc,feature=1,whichAssay=1))
   expect_silent(plotFeatureBoxplot(object=multi_cc,feature=1,whichAssay=2))
 
-  expect_silent(plotHeatmap(multi_cc, whichAssay=1))
-  expect_silent(plotHeatmap(multi_cc, whichAssay=2))
+  small_cc <- multi_cc[1:10,c(1:3, 290:293)]
+  small_cc <- makeDendrogram(small_cc)
+  expect_silent(plotHeatmap(small_cc, whichAssay=1))
+  expect_silent(plotHeatmap(small_cc, whichAssay=2))
 
 })
