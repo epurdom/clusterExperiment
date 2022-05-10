f8ea90e195ab48166be3dbd7e7bd6ea1ef222284
Elizabeth Purdom <epurdom@stat.berkeley.edu>
2022-05-10 15:46:52 -0700

    move files

--- RSECPackage/R/plotExtensions.R
+++ RSECPackage/R/plotExtensions.R
@@ -17,3 +17,20 @@ setMethod(
     callNextMethod(object,whichClusters=whichClusters)
 	}
 )
+
+#' @name plotHeatmap
+#' @title Extend plotHeatmap to RSECClass
+#
+#' @rdname plotHeatmap
+#' @aliases plotHeatmap,RSECClass-method
+setMethod(
+  f = "plotHeatmap",
+  signature = signature(data = "RSECClass"),
+  definition = function(data,
+                        whichClusters= c("primary","workflow","all","none"),
+                        ...
+  ){
+    whCl<-getClusterIndex(data,whichClusters=whichClusters,noMatch="silentlyRemove")
+		callNextMethod(data,whichClusters=whCl)
+		
+})
